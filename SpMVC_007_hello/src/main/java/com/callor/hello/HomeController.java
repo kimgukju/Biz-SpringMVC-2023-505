fbca71323d957191114be31962ec531ace78d428
kimgukju <kimgukju@kimgukju.com>
2023-09-21 15:42:49 +0900

    0921

--- SpMVC_007_hello/src/main/java/com/callor/hello/HomeController.java
+++ SpMVC_007_hello/src/main/java/com/callor/hello/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -19,5 +20,13 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		return "home";
 	}
+	
+//	@ResponseBody
+//	@RequestMapping(value="/home", method=RequestMethod.GET )
+//	public String home() {
+//		return "Hello Home";
+//	}
+	
+	
 	
 }
